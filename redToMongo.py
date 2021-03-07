c97e06ebf3d1b4065b40f2f3c8c2cd02e1ab522b
JasonVer <74768820+JasonVer@users.noreply.github.com>
2021-03-07 14:46:45 +0100

    Update redToMongo.py

--- redToMongo.py
+++ redToMongo.py
@@ -29,8 +29,8 @@ def ToMongo(con, DataInBase):
     #Fill in variables
     hashes = list(map(str, con.lrange("Hash", 0, -1)))
     time = list(map(str, con.lrange("Time", 0, -1)))
-    bitcoin = list(map(float, con.lrange("Bitcoin", 0, -1)))
-    usd = list(map(float, con.lrange("US Dollar", 0, -1)))
+    bitcoin = list(map(str, con.lrange("Bitcoin", 0, -1)))
+    usd = list(map(str, con.lrange("US Dollar", 0, -1)))
 
     #Pass on values
     mon = max(usd)
